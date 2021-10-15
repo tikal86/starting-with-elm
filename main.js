5adb115cd24430fbb97a64a34969841a641a513a
andre hogenkamp <tikaltikal@gmail.com>
2021-10-15 23:19:00 +0200

    Electron works, require does not

---
+++ main.js
@@ -0,0 +1,42 @@
+'use strict'
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+let mainWindow // saves a global reference to mainWindow so it doesn't get garbage collected
+
+app.on('ready', createWindow) // called when electron has initialized
+
+// This will create our app window, no surprise there
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+        preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: true, 
+        contextIsolation: false
+    }
+  })
+
+  // display the index.html file
+  mainWindow.loadURL(`file://${ __dirname }/src/static/index.html`)
+  
+  // open dev tools by default so we can see any console errors
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+/* Mac Specific things */
+
+// when you close all the windows on a non-mac OS it quits the app
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') { app.quit() }
+})
+
+// if there is no mainWindow it creates one (like when you click the dock icon)
+app.on('activate', () => {
+  if (mainWindow === null) { createWindow() }
+})
